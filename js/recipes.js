617fc696da5881ec5ca3f9bec939a417ae174b26
An Pham <anpham.0992@gmail.com>
2025-06-19 12:46:21 -0400

    add pause and play features

--- js/recipes.js
+++ js/recipes.js
@@ -1,4 +1,26 @@
-function playSound(speakerElement) {
-    var audio = speakerElement.parentElement.querySelector("audio");
-    audio.play();
+function toggleAudio(speakerElement) {
+    const container = speakerElement.parentElement;
+    const audio = container.querySelector("audio");
+
+    if (audio.paused) {
+        // Pause all other audios
+        const allAudios = document.querySelectorAll("audio");
+        allAudios.forEach(a => {
+            if (a !== audio) {
+                a.pause();
+                a.currentTime = 0;
+            }
+        });
+
+        audio.play();
+        speakerElement.classList.add("playing");
+    } else {
+        audio.pause();
+        speakerElement.classList.remove("playing");
+    }
+
+    // Reset icon when audio ends
+    audio.onended = () => {
+        speakerElement.classList.remove("playing");
+    };
 }
