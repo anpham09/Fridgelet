a8d470b950c21710f66a4599794e449623a6af25
An Pham <anpham.0992@gmail.com>
2025-06-20 18:23:19 -0400

    almost done

--- js/index.js
+++ js/index.js
@@ -2,18 +2,18 @@ const fridge = document.getElementById("fridge");
 const food = document.getElementsByClassName("food_container");
 
 
-let isOpen = false;
+// let isOpen = false;
 
-fridge.addEventListener("click", () => {
-    isOpen = !isOpen;
-    fridge.src = isOpen
-      ? "../img/fridge/opened_fridge.png"
-      : "../img/fridge/closed_fridge.png";
+// fridge.addEventListener("click", () => {
+//     isOpen = !isOpen;
+//     fridge.src = isOpen
+//       ? "../img/fridge/opened_fridge.png"
+//       : "../img/fridge/closed_fridge.png";
 
-    for (let i = 0; i < food.length; i++) {
-        food[i].style.display = isOpen ? "block" : "none";
-    }
-});
+//     for (let i = 0; i < food.length; i++) {
+//         food[i].style.display = isOpen ? "block" : "none";
+//     }
+// });
 
 document.querySelectorAll('.remove_btn').forEach(button=>{
   button.addEventListener('click', ()=> {
@@ -24,13 +24,13 @@ document.querySelectorAll('.remove_btn').forEach(button=>{
 
     //Add plus button
     const plus = document.createElement('button');
-    plus.innerText = '+';
+    plus.innerText = 'Deleted';
     plus.className = 'plus_btn';
   
     // Transfer to add_recipe_form page after clicked
-    plus.addEventListener('click', ()=>{
-      window.location.href = '../html/add_recipe_form.html';
-    });
+    // plus.addEventListener('click', ()=>{
+    //   window.location.href = '../html/add_recipe_form.html';
+    // });
   
     foodItem.appendChild(plus);
   })
